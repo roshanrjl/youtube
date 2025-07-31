10dfd2bba981c5cf139489dd34411b57e2cf5855
roshanrjl <roshanrijal700@gmail.com>
2025-07-31 13:09:05 +0545

    feat: implement API clients for comments, dashboard, liked, playlist, subscription, tweets, and user with token authorization

--- client/src/api/commentsApi/commentapi.jsx
+++ client/src/api/commentsApi/commentapi.jsx
@@ -21,3 +21,39 @@ apiClient.interceptors.request.use(
     return Promise.reject(error);
   }
 );
+
+export const getVideoComments = (videoId)=>{
+ return  apiClient.get(`comment/${videoId}`)
+}
+
+export const addComment = (videoId)=>{
+return apiClient.post(`comment/${videoId}`)
+}
+
+export const updateComment = (commentId)=>{
+  return apiClient.patch(`comment/${commentId}`)
+}
+
+export const deleteComment = (commentId)=>{
+  return apiClient.delete(`comment/${commentId}`)
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ client/src/api/likedApi/likedapi.jsx
@@ -0,0 +1,21 @@
+import axios from "axios";
+import { Localstorage } from "../../utils";
+
+const apiClient = axios.create({
+  baseURL: import.meta.env.VITE_SERVER_URI,
+  withCredentials: true,
+  timeout: 120000,
+});
+
+apiClient.interceptors.request.use(
+  function (config) {
+    const token = Localstorage.get("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  }
+);
